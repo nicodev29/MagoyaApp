7c81585a59654d03f6c0dccd2ef2a494b2c093a4
Nicolas <nicogarcia1229@gmail.com>
2024-04-27 00:10:18 -0300

    first

---
+++ src/main/java/com/example/magoyaapp/MagoyaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.magoyaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagoyaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagoyaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/magoyaapp/MagoyaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magoyaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagoyaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
